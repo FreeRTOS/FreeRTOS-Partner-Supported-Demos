06af2c9e0d52b92bd7cc4a300ebda24dfc83286d
vung.tran.zg <vung.tran.zg@renesas.com>
2025-09-16 16:27:38 +0700

    Added new Demo projects for RH850 U2Cx CCRH and GHS.

---
+++ RH850_U2Cx_CCRH/U2Cx_FreeRTOS_Demo_MultiCore/bsp/bsp_irq/bsp_irq.c
@@ -0,0 +1,18 @@
+#include "bsp_common.h"
+
+// extern irq_vector_t g_vector_table_PE0[RTOS_VECTOR_TABLE_MAX_ENTRIES];
+// extern irq_vector_t g_vector_table_PEn[RTOS_VECTOR_TABLE_MAX_ENTRIES];
+
+
+// void vCommonISRHandler (int irq)
+// {
+//     if (GetCoreID() == 0)
+//     {
+//         g_vector_table_PE0[irq]();
+//     }
+//     else if (GetCoreID() == 1)
+//     {
+//         g_vector_table_PEn[irq]();
+//     }
+// }
+
---
+++ RH850_U2Cx_GHS/U2Cx_FreeRTOS_Demo_SingleCore/startup/make/ghs/startup_rh850_U2Cx_defs.mak
@@ -0,0 +1,6 @@
+################################################################################
+# REGISTRY
+#
+CC_INCLUDE_PATH += U:\startup\include
+CC_SRC_PATH += U:\startup\include
+CPP_INCLUDE_PATH +=
